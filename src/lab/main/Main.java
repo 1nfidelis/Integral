5d0a629b75642ef3528ae23cf5c0330d2d7da7c7
infidelis <vgus38@ya.ru>
2018-10-21 20:27:24 +0700

    Initial commit

---
+++ src/lab/main/Main.java
@@ -0,0 +1,46 @@
+package lab.main;
+
+
+import lab.integral.Integral;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+
+/*
+Функция на отрезке [a,b]:                   Math.sin(x)                                               Math.sqrt(x)
+Производная :                    -Math.cos(toPoint) + Math.cos(fromPoint)          1/(2*Math.sqrt(toPoint)) - 1/(2*Math.sqrt(fromPoint))
+ */
+
+public class Main {
+
+    public static double func (double x) {
+        return Math.sin(x);
+    }
+
+    public static double funcDerivative (double fromPoint, double toPoint) {
+        return  -Math.cos(toPoint) + Math.cos(fromPoint);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        FileWriter gauss = new FileWriter("/Users/infidelis/IdeaProjects/Integral/src/lab/files/gauss.txt");
+        FileWriter square = new FileWriter("/Users/infidelis/IdeaProjects/Integral/src/lab/files/square.txt");
+        FileWriter trapeze = new FileWriter("/Users/infidelis/IdeaProjects/Integral/src/lab/files/trapeze.txt");
+
+        Integral integral = new Integral(0,1, 100);
+
+        integral.changeTill(10); // КОЛИЧЕСТВО ЦИКЛОВ ПОГРЕШНОСТИ
+
+        System.out.println( "Прямоугольньники: " + integral.square());
+        System.out.println( "Трапеции: " + integral.trapeze());
+        System.out.println( "Гаусс: " + integral.gauss());
+
+        integral.errorGauss(gauss);
+        integral.errorSquare(square);
+        integral.errorTrapeze(trapeze);
+
+
+    }
+}
